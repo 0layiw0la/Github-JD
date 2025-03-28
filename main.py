49f5bbdd7b0a2163d57e7864f266358662f40b16
Katrina Emegbagha <abdulrahimibrahimolayiwola@gmail.com>
2025-03-28 07:33:01 +0100

    fixing render sleep issue

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from app import create_app
+import threading
+import requests
+import time
 
 flask_app = create_app()
 
+def keep_alive():
+    while True:
+        try:
+            requests.get("hhttps://github-comparison.onrender.com/")
+            print("Pinged the app to keep it alive!")
+        except Exception as e:
+            print(f"Error pinging: {e}")
+        time.sleep(600)  # Ping every 10 minutes
+
+# Start keep_alive() in a separate thread
+threading.Thread(target=keep_alive, daemon=True).start()
+
 if __name__ == "__main__":
     flask_app.run()
-
