69b2685c6d9724d7d900dcec3796efc16d3ed625
Abdulrahim Ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2025-04-23 23:41:14 +0100

    added routes for edit and add

--- routes.py
+++ routes.py
@@ -282,4 +282,20 @@ def register_routes(app,db):
         of project tracking functionality.
     """
     
-    
+    from flask import request
+
+    @app.route('/edit_project')
+    def edit_project():
+        name = request.args.get('name')
+        bullets = request.args.get('bullets')
+        if bullets:
+            bullets = json.loads(bullets)
+        return (name,bullets)
+
+    @app.route('/add_bullet')
+    def add_bullet():
+        name = request.args.get('name')
+        bullets = request.args.get('bullets')
+        if bullets:
+            bullets = json.loads(bullets)
+        return (name,bullets)
