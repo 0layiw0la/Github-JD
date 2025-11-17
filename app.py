6d42a4ac61bfcc57908008a5b1f36d1710828f3b
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2025-11-17 03:21:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import itertools
 load_dotenv() 
 
 DATABASE = os.environ.get("SQL_DATABASE_URL")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 db = SQLAlchemy()
@@ -16,7 +17,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__,template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-    app.secret_key = "your_secret_key_here"
+    app.secret_key = SECRET_KEY
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
     
     app.jinja_env.filters['fromjson'] = json.loads
     app.jinja_env.filters['zip'] = lambda *seqs: list(itertools.zip_longest(*seqs, fillvalue=None))
